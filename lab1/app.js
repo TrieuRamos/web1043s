b771c582b53bdf1531d7e7fdc08de702cfe6c0ef
TrieuRamos <trieutvpd07435@fpt.edu.vn>
2023-03-08 17:15:13 +0700

    first commit

---
+++ lab1/app.js
@@ -0,0 +1,11 @@
+// javaScipt for lab1
+let a = prompt("Moi ban nhap so nguyen A:");
+// let a1 = parseInt(a);
+
+let b = prompt("Moi ban nhap so nguyen B:");
+// let b1 = parseInt(b);
+
+let c = parseInt(a) + parseInt(b);
+let x = a + b;
+document.write(c);
+console.log(x);
