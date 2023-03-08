8c3f5b5221cfa1525dfe3a646888ba7eda195866
TrieuRamos <trieutvpd07435@fpt.edu.vn>
2023-03-08 22:06:38 +0700

    slab1

--- lab1/app.js
+++ lab1/app.js
@@ -1,11 +1,17 @@
 // javaScipt for lab1
-let a = prompt("Moi ban nhap so nguyen A:");
-// let a1 = parseInt(a);
-
-let b = prompt("Moi ban nhap so nguyen B:");
-// let b1 = parseInt(b);
-
+//khai bao bien a va nhap gia tri tu bam phim
+//nhap so nguyen a vao bam phim
+let a = prompt("Mời bạn nhập số nguyên A:");
+let a1 = parseInt(a);
+//nhap so nguyen b vao bam phim
+let b = prompt("Mời bạn nhập số nguyên B:");
+let b1 = parseInt(b);
+//tinh tong cua a va 
 let c = parseInt(a) + parseInt(b);
 let x = a + b;
-document.write(c);
+//in ra ket qua bang 2 cach
+document.write("Tổng của " + a + " và " + b + " là: " + c);
 console.log(x);
+
+
+
