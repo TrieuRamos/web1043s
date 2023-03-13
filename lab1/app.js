86bb479d6fc0f8b8b11968a91d2a2b099df754d7
TrieuRamos <trieutvpd07435@fpt.edu.vn>
2023-03-13 10:26:49 +0700

    lab3

--- lab1/app.js
+++ lab1/app.js
@@ -3,12 +3,15 @@
 //nhap so nguyen a vao bam phim
 let a = prompt("Mời bạn nhập số nguyên A:");
 // let a1 = parseInt(a);
+
 //nhap so nguyen b vao bam phim
 let b = prompt("Mời bạn nhập số nguyên B:");
 // let b1 = parseInt(b);
+
 //tinh tong cua a va b
 let c = parseInt(a) + parseInt(b);
 let x = a + b;
+
 //in ra ket qua bang 2 cach
 document.write("Tổng của " + a + " và " + b + " là: " + c);
 console.log(x);
