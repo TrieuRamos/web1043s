06691cdbb59d60d9f50c172db2df2bae7b33d1d8
TrieuRamos <trieutvpd07435@fpt.edu.vn>
2023-03-13 10:24:48 +0700

    hello

--- lab1/app.js
+++ lab1/app.js
@@ -2,10 +2,10 @@
 //khai bao bien a va nhap gia tri tu bam phim
 //nhap so nguyen a vao bam phim
 let a = prompt("Mời bạn nhập số nguyên A:");
-let a1 = parseInt(a);
+// let a1 = parseInt(a);
 //nhap so nguyen b vao bam phim
 let b = prompt("Mời bạn nhập số nguyên B:");
-let b1 = parseInt(b);
+// let b1 = parseInt(b);
 //tinh tong cua a va b
 let c = parseInt(a) + parseInt(b);
 let x = a + b;
